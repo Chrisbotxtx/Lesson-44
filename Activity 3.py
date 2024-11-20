fed0db9b166f23a6323b923133bf492193e7764e
Chrisbotxtx <christopher.mwakasegecmj@gmail.com>
2024-11-20 19:52:46 +0300

    Activities for lesson 44

---
+++ Activity 3.py
@@ -0,0 +1,8 @@
+set1={"Ford","Toyota","Nissan","Range rover"}
+print("Set1 is",set1)
+
+set2={"Nissan","Ford","Fiat"}
+print("Set2 is",set2)
+
+set3=set1.intersection(set2)
+print("The intersection of set1 and set2 is",set3)
---
+++ Activity 4.py
@@ -0,0 +1,8 @@
+set1={"Ford","Toyota","Nissan","Range rover"}
+print("Set1 is",set1)
+
+set2={"Nissan","Ford","Fiat"}
+print("Set2 is",set2)
+
+set3=set1.Union(set2)
+print("The Union of set1 and set2 is",set3)
